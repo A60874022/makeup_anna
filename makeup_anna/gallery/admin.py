650bcdbf9cef622ed7ef2f6fb0d3b2846749527b
Anton <109154319+A60874022@users.noreply.github.com>
2023-04-27 19:10:22 +0500

    Update admin.py

--- makeup_anna/gallery/admin.py
+++ makeup_anna/gallery/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from gallery.models import Gallery, Tag
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug',)
+    search_fields = ('name',)
+    list_filter = ('name',)
+
+
+@admin.register(Gallery)
+class GalleryAdmin(admin.ModelAdmin):
+    list_display = ('tags', 'image', 'pub_date')
+    search_fields = ('tags',)
+    list_filter = ('pub_date',)
